3b1b3d22de71144532a307950855d8d64b8a8b22
lighter <apple.xva@gmail.com>
2018-07-14 22:02:59 +0800

    finish static list with recycler view

---
+++ app/src/main/java/com/example/lighter/listmaker/ListSelectionViewHolder.kt
@@ -0,0 +1,10 @@
+package com.example.lighter.listmaker
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+
+class ListSelectionViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView!!) {
+    val listPosition = itemView?.findViewById(R.id.itemNumber) as TextView
+    val listTitle = itemView?.findViewById(R.id.itemString) as TextView
+}
--- app/src/main/java/com/example/lighter/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/lighter/listmaker/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.lighter.listmaker
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 
@@ -10,15 +13,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var listsRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
+        listsRecyclerView = findViewById(R.id.lists_recycleview)
+        listsRecyclerView.layoutManager = LinearLayoutManager(this)
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
